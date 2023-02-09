103613f2ca2b5bb7700a00c39b5b39385bda9c34
AnesKostreba <aneskostreba@yahoo.com>
2023-02-09 17:17:09 +0100

    add Movie-form

--- Movie-form/JS/form.js
+++ Movie-form/JS/form.js
@@ -1,1 +1,15 @@
-console.log("eee");
+var neki = [];
+$('#addMovie').on('click', function () {
+    var numberInput = Number($("#length").val());
+    var genre = '';
+    neki.push(numberInput);
+    $('select option:selected').each(function () {
+        genre = $(this).val();
+    })
+    var name = $('#name').val();
+    var movie = new Movie(name, numberInput, genre);
+    $('.result').append(`<tr><td>${movie.getData()}</td></tr>`);
+    $('.lengthMovies').append(movie.result());
+    $('#movieSel').append(`<option>${movie.getName()}</option>`);
+    $('#form')[0].reset();
+});
